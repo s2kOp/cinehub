2f06a4cd155d19f4d71d21fa8e0def41eca57c85
rohitrn_ <rohitrajeev05@gmail.com>
2025-06-20 02:44:43 +0530

    my first commit

--- backend/app.py
+++ backend/app.py
@@ -11,11 +11,9 @@ def get_recommendations():
     results = recommend(title)
     return jsonify({"recommendations": results})
 
-@app.route("/")
-def home():
-    return "CineHub backend is live!"
+
 
 if __name__ == "__main__":
     import os
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
